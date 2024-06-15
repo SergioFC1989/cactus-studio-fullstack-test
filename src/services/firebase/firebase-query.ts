44f96576006ad8969882c2fdcd065acea946c91d
SergioFC1989 <sergiofc@hotmail.es>
2024-06-16 01:58:32 +0200

    feat: methods created to obtain the points and materials

--- src/services/firebase/firebase-query.ts
+++ src/services/firebase/firebase-query.ts
@@ -5,10 +5,12 @@ import { CollectionName } from "./type";
 export const firebaseQuery = async (collectionName: CollectionName) => {
   try {
     const querySnapshot = await getDocs(collection(db, collectionName));
-    const data = querySnapshot.docs.map((doc) => doc.data());
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      data: doc.data(),
+    }));
     return data;
   } catch (error) {
-    console.error("Error fetching data from Firestore: ", error);
     throw new Error("Error fetching data from Firestore");
   }
 };
