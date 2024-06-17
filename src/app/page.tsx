ddfad05b57917c72eda95af6c38e683961f96424
Sergio <52042600+SergioFC1989@users.noreply.github.com>
2024-06-17 12:14:16 +0200

    Update page.tsx
    Removed innecesary line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import { ImageLayoutComponent } from "@/components/ImageLayoutComponent";
-import { Suspense } from "react";
 
 const ImageLayout = () => (
   <div className="w-full h-full">
-    <Suspense
-      fallback={
-        <div className="absolute bg-green w-full h-full">
-          HOLAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </div>
-      }
-    >
       <ImageLayoutComponent />
-    </Suspense>
   </div>
 );
 
